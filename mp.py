ec52d73c29c734a8124ed9ff8efcfd0c4ed633c3
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-06 20:06:27 +0100

    okay wooo it works theres a liitle spec i forgot to color but its all good

--- mp.py
+++ mp.py
@@ -1,12 +1,13 @@
 import pygame
 pygame.init()
-width=1920
-height=1080
+width = 1528
+height = 1080
 canvas=pygame.display.set_mode((width,height))
 pygame.display.set_caption("intro")
 fps=pygame.time.Clock()
 bg=pygame.image.load("Textures/Untitled design (20).png")
 bg = pygame.transform.rotate(bg, 90)
+bg = pygame.transform.scale(bg, (width, height))
 car = pygame.image.load("Textures/RGBBlackCar.png").convert_alpha()
 car=pygame.transform.scale(car,(50,50))
 carx=300
@@ -15,8 +16,9 @@ running=True
 while running:
     for event in pygame.event.get():
         if event.type ==pygame.QUIT:
-            running=False
-    canvas.blit(bg,(0,0))           
+            running=False 
+
+    canvas.blit(bg, (0,0))         
     canvas.blit(car, (carx, cary))
     pygame.display.flip()
     fps.tick(60)
