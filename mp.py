94d40b3c3a9ec787e313c4578d1ac1b1dfe8d511
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-06 17:30:24 +0100

    mostly just defining stuff and adding graphics and stuff

---
+++ mp.py
@@ -0,0 +1,8 @@
+import pygame
+pygame.init
+width=1920
+height=1080
+canvas=pygame.display.set_mode((width,height))
+pygame.display.set_caption("intro")
+fps=pygame.time.Clock()
+bg=pygame.image.load("Textures\mainpage bg.svg")
