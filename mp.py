df15a1dce5f1f20b6606f3d2c2a9c837b51283ed
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-06 19:49:20 +0100

    loads of variables and assigned the size of the car up next is acc tryna see the car and everything

--- mp.py
+++ mp.py
@@ -6,3 +6,9 @@ canvas=pygame.display.set_mode((width,height))
 pygame.display.set_caption("intro")
 fps=pygame.time.Clock()
 bg=pygame.image.load("Textures\mainpage bg.svg")
+car = pygame.image.load("Textures\RGBBlackCar.png").convert_alpha()
+car=pygame.transform.scale(car,(50,50))
+carx=300
+cary=500
+running=True
+           
