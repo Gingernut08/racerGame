b7e97e44165b6bc89c415618b7f3623c24a9020b
Gingernut08 <oliver.spam.08@gmail.com>
2026-09-10 17:30:52 +0100

    Modded some vars in mp.py and it gone funny sayin i made new files, i pinky promise i didn

---
+++ mp.py
@@ -0,0 +1,28 @@
+import pygame
+pygame.init()
+width = 1920
+height = 1080
+canvas=pygame.display.set_mode((width,height), pygame.FULLSCREEN)
+pygame.display.set_caption("intro")
+fps=pygame.time.Clock()
+bg=pygame.image.load("Textures/Untitled design (20).png")
+bg = pygame.transform.rotate(bg, 90)
+bg = pygame.transform.scale(bg, (width, height))
+car = pygame.image.load("Textures/RGBBlackCar.png").convert_alpha()
+car=pygame.transform.scale(car,(50,50))
+carx=300
+cary=500
+running=True
+while running:
+    for event in pygame.event.get():
+        if event.type ==pygame.QUIT:
+            running=False 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+    canvas.blit(bg, (0,0))         
+    canvas.blit(car, (carx, cary))
+    pygame.display.flip()
+    fps.tick(60)
+pygame.quit()
