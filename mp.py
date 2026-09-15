bcb25eb70e70377f2c61bdfb341631d1c68089da
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-15 22:36:09 +0100

    umm so i forgot brackets outside the brackets , added those but ummm why tf did my screen just black out like my laptop was dying wtf, istg its not that old.

--- mp.py
+++ mp.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-screen=pygame.display.set_mode(0,0),pygame.FULLSCREEN
+screen=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 (WIDTH,HEIGHT)=screen.get_size()
 screen=pygame.display.set_mode((WIDTH,HEIGHT), pygame.FULLSCREEN)
 pygame.display.set_caption("intro")
