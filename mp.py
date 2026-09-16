7ce8c35be51506f39e13143e5f9637b022c8c44a
Gingernut08 <oliver.spam.08@gmail.com>
2026-09-16 06:44:40 +0100

    idfk it made me commit

--- mp.py
+++ mp.py
@@ -1,9 +1,9 @@
 import pygame
 pygame.init()
-WIDTH = 1920
-HEIGHT = 1080
-screen=pygame.display.set_mode((WIDTH,HEIGHT), pygame.FULLSCREEN)
+screen=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+(WIDTH,HEIGHT)=screen.get_size()
 pygame.display.set_caption("intro")
+
 clock=pygame.time.Clock()
 bg=pygame.image.load("Textures/Untitled design (20).png")
 bg = pygame.transform.rotate(bg, 90)
