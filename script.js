1b7f3b67c56ff7b011c3648431abb196ddf27fc2
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-20 21:06:52 +0100

    they just disapeared istfg

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const car=[];
 const no=6;
 for(let i=0;i<no;i++){
+    const trail=document.createElement("div")
     const car2=document.createElement("img");
+    trail.className="trcar"
     car2.src="Textures/car/RGBBlackCar.png";
     car2.className="car";
     //temporariliy here, will be altered based on where it goes later
-    car2.style.left=Math.random()*100+"%";
-    car2.style.top=19+Math.random()*45+"%";
-    document.body.appendChild(car2);
-    car.push(car2);
+    trail.style.left=Math.random()*100+"%";
+    trail.style.top=19+Math.random()*45+"%";
+    document.body.appendChild(trail);
+    car.push(trail);
 }   
 //i like to mov it move it 
 function move(){
