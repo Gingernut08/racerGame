fb3b0f91dc4eb08c54ab3459e513bc1ceae92c25
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-20 21:39:17 +0100

    its giving svervy ants

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ for(let i=0;i<no;i++){
     //temporariliy here, will be altered based on where it goes later
     trail.style.left=(i*20)+"%";
     trail.style.top=19+Math.random()*40+"%";
-    trail.speed=0.3+Math.random()*0.7;
+    trail.speed=0.3+Math.random()*0.7;    
+    trail.lane=parseFloat(trail.style.top);
+    trail.s1=0.01+Math.random()*0.04;
+    trail.s2=Math.random()*100;
     trail.appendChild(car2);
     document.body.appendChild(trail);
     car.push(trail);
@@ -20,17 +23,21 @@ function move(){
         //hmmm trafficc behaviour
         let x=parseFloat(car[i].style.left);
         let sp=car[i].speed;
+        car[i].s2+=car[i].s1;
+        let s5=Math.sin(car[i].s2)*3;
+        car[i].style.top=(car[i].lane+s5)+"%";
         for(let j=0;j<car.length;j++){
             if(i===j)continue;
             let pt1=parseFloat(car[j].style.left);
             let gap=pt1-1;
             if(gap>0 && gap<3){
-                sp*=0.3;
+                sp*=0.15;
             }
         }
         x+=sp;
         if(x>100){
             x=-10;
+            car[i].lane=19+Math.random()*40;
         }
         car[i].style.left=x+"%";
     }
