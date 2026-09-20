91461d09b0687d3e45b891a1ce889fd4567cc459
Gingernut08 <oliver.spam.08@gmail.com>
2026-09-20 22:17:11 +0100

    added a python scrip which will run PyInstaller to build an exe, also added these files to .gitignore

---
+++ build.py
@@ -0,0 +1,15 @@
+import subprocess, sys
+
+subprocess.run([
+    sys.executable,
+    "-m", "PyInstaller",
+    "--onedir",
+    "--windowed",
+    "--add-data", "Textures;Textures",
+    "--add-data", "Saves;Saves",
+    "--add-data", "audio;audio",
+    "main.py"
+], check = True)
+
+print("\nBuild Complete")
+print("Executable: dist\\main\\main.exe")
