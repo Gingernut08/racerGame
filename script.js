9d804bc191e49cb7a5a7b9286ee8aa01fa676116
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-20 20:38:27 +0100

    ahahah yess it workssss omgg so happy , the issue u ask semi colons woo

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const car=[];
 const no=6;
-const top=30;
-const bot=65;
 for(let i=0;i<no;i++){
-    const car2=document.getElementById("img");
-    car2.src="Textures/car/RGBBlackCar.png"
+    const car2=document.createElement("img");
+    car2.src="Textures/car/RGBBlackCar.png";
     car2.className="car";
     //temporariliy here, will be altered based on where it goes later
     car2.style.left=Math.random()*100+"%";
-    car2.style.top=top+Math.random()*(bot-top)+"%";
+    car2.style.top=19+Math.random()*45+"%";
     document.body.appendChild(car2);
-    car.push(car2)
+    car.push(car2);
 }
 //i like to mov it move it 
 function move(){
@@ -18,9 +16,11 @@ function move(){
         let x=parseFloat(car[i].style.left);
         x+=0.2;
         if(x>100){
-            x=-10
+            x=-10;
         }
-        car[i].style.left=x+"%"
+        car[i].style.left=x+"%";
     }
     requestAnimationFrame(move);
 }
+move();
+
