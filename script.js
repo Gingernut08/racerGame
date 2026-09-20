98c2f91fa5e31a66ac455aced129fe1104077ff8
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-20 20:09:15 +0100

    added a cool cursor idk if i said it already but thats what i did

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const car=[];
 const no=6;
-const top=30;
-const bot=65;
+const top=0;
+const bot=100;
 for(let i=0;i<no;i++){
     const car2=document.createElement("img");
     car2.src="Textures/car/RGBBlackCar.png"
     car2.className="car";
     //temporariliy here, will be altered based on where it goes later
     car2.style.left=Math.random()*100+"%";
-    car2.style.top=top+Math.random()*(bot-top)+"%";
+    car2.style.top=(top+Math.random()*(bot-top))+"%";
     document.body.appendChild(car2);
     car.push(car2)
 }
